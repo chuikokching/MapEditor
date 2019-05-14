034117e949d2a70600387ead32ba4225ddda7678
chuikokching <guozheng.xu@stud.uni-due.de>
2019-05-14 14:38:55 +0200

    Update.

---
+++ OSMwebapp/XMLGenerator.js
@@ -0,0 +1,6 @@
+function createXML(){
+    var txt="<?xml version="+'"'+"1.0"+'"'+ " encoding=" + '"'+"ISO-8859-1"+ '"' + "?>" +"&#x000A;";
+    txt=txt+"<leaflet_data>";
+    txt=txt+"</leaflet_data>"
+    alert(txt);
+}
