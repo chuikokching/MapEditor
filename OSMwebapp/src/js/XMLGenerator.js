f2a3757941dd3151789e268162a47399ab1b86d3
chuikokching <guozheng.xu@stud.uni-due.de>
2019-05-22 18:17:42 +0200

    Aufgabe1,2 Verbesserung

--- OSMwebapp/src/js/XMLGenerator.js
+++ OSMwebapp/src/js/XMLGenerator.js
@@ -1,4 +1,4 @@
-function createXMLString(textContent,arrayOption,arrayAnswer,map_parameter){
+function createXMLString2(textContent,arrayOption,arrayAnswer,map_parameter){
     var length;
     var txt="<?xml version="+'"'+"1.0"+'"'+ " encoding=" + '"'+"ISO-8859-1"+ '"' + "?>"+'\n';
     txt=txt+"<leaflet_data>"+'\n';
@@ -52,6 +52,44 @@ function createXMLString(textContent,arrayOption,arrayAnswer,map_parameter){
         txt=txt+"          </Coordinate>"+'\n';
         txt=txt+"          <Answer>"+ "true" +"</Answer>"+'\n';
     }
+    txt=txt+"       </Aufgabe>"+'\n';
+    txt=txt+"</leaflet_data>";
+    //alert(txt);
+    return txt;
+}
+
+
+
+
+function createXMLString1(textContent,array,map_parameter){
+    var length;
+    var txt="<?xml version="+'"'+"1.0"+'"'+ " encoding=" + '"'+"ISO-8859-1"+ '"' + "?>"+'\n';
+    txt=txt+"<leaflet_data>"+'\n';
+    txt=txt+"       <Aufgabe>"+'\n';
+    txt=txt+"          <Aufgabestellung>"+textContent+"</Aufgabestellung>"+'\n';
+    txt=txt+"          <View_Coordinate>"+'\n';
+    txt=txt+"              <lat>"+map_parameter[0]+"</lat>"+'\n';
+    txt=txt+"              <lng>"+map_parameter[1]+"</lng>"+'\n';
+    txt=txt+"          </View_Coordinate>"+'\n';
+    txt=txt+"          <Zoom>"+map_parameter[2]+"</Zoom>"+'\n';
+    for(let i=0;i<array.length;i++)
+    {
+        txt=txt+"          <Type>"+getGeoJsonType(array[i])+"</Type>"+'\n';
+        txt=txt+"          <Coordinate>"+'\n';
+
+        if(getGeoJsonType(array[i])=="Polygon")
+        {
+            length=array[i].geometry.coordinates[0].length;
+
+            for(let j=0;j<length;j++)
+            {
+                txt=txt+"              <lat>"+ array[i].geometry.coordinates[0][j][0]+"</lat>"+'\n';
+                txt=txt+"              <lng>"+ array[i].geometry.coordinates[0][j][1]+"</lng>"+'\n';
+            }
+        }
+        txt=txt+"          </Coordinate>"+'\n';
+    }
+
     txt=txt+"       </Aufgabe>"+'\n';
     txt=txt+"</leaflet_data>";
     //alert(txt);
